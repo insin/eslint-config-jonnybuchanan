5e6be90d7c31816a5db90e7e353baeef7691e5ee
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-11-16 21:21:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![npm package][npm-badge]][npm]
+
+A personal ESLint config, but with ESLint itself and all the necessary shareable config, plugins and parser as dependencies.
+
+Provides a `lint` command which runs ESLint with this module's `.eslintrc` in the context of this module's dependencies, so I don't have to manage these dependencies in every project.
+
+## MIT Licensed
+
+[npm-badge]: https://img.shields.io/npm/v/eslint-config-jonnybuchanan.svg
+[npm]: https://www.npmjs.org/package/eslint-config-jonnybuchanan
---
+++ bin/lint.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var execSync = require('child_process').execSync
+var fs = require('fs')
+var path = require('path')
+
+// Find eslint .bin script
+var options = [
+  // Global install or npm@2 local install, dependencies in local node_modules
+  path.join(__dirname, '../node_modules/.bin/eslint'),
+  // Local npm@3 install, .bin and dependencies are siblings
+  path.join(__dirname, '../../.bin/eslint')
+]
+
+var eslint
+for (var i = 0; i < options.length; i++) {
+  try {
+    fs.statSync(options[i])
+    eslint = options[i]
+    break
+  }
+  catch (e) {
+    // pass
+  }
+}
+
+if (!eslint) {
+  console.error('Unable to locate .bin/eslint')
+  process.exit(1)
+}
+
+var command =
+  [eslint, '-c', path.join(__dirname, '../.eslintrc')]
+    .concat(process.argv.slice(2))
+    .join(' ')
+
+execSync(command, {stdio: [0, 1, 2]})
